5163f76795cfc225eb9cf4c62816f5f4daac0aa8
galihpermana29 <galihcbn123@gmail.com>
2021-02-03 18:42:39 +0700

    Event Handler on React

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,25 @@
 const Home = () => {
-   return (  
-      <div className="home">
-         <h2>Homepage</h2>
-      </div>
-   );
-}
- 
+	const handleClickOne = (e) => {
+		console.log('No argument passed to this function, Hello!', e);
+	};
+
+   const handleClickTwo = (userName, e) => {
+      console.log(`Hello, ${userName}, there is an argument passed to this function`, e)
+   }
+
+
+
+
+	return (
+		<div className="home">
+			<h2>Homepage</h2>
+			{/* if the function doesnt passing an argument */}
+			<button onClick={handleClickOne}>Click Me</button>
+			<button onClick={(e) => {
+            handleClickTwo('Galih', e)
+         }}>Click Me Again</button>
+		</div>
+	);
+};
+
 export default Home;
