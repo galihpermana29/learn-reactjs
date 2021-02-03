08948fec54489e2f918ec35c4048918833b54447
galihpermana29 <galihcbn123@gmail.com>
2021-02-03 20:14:55 +0700

    useState hook

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,25 @@
+import { useState } from 'react';
 const Home = () => {
+	// let name = 'Galih';
+   console.log(useState('Galih'))
+   
+   // Menggunakan useState hook, jadi ketika di klik, akan merubah data yang ada pada htmlnya (misalnya begitu). nah sesuatu yang seperti itu membutuhkan variabel yang reactive, sehingga ketika terjadi perubahan akan merender ulang template html yang sudah ada.
+
+   // import {useState} from 'react', lalu gunakan useState() seperti function biasa, yang mana dia mereturn sebuah array, value yang pertama adalah parameternya dan value yang kedua adalah function yang bisa merubah value yang pertama itu. seperti di bawah ini
+   let [name, setName] = useState('Galih');
 	const handleClickOne = (e) => {
-		console.log('No argument passed to this function, Hello!', e);
+		setName('Permana');
 	};
-
-   const handleClickTwo = (userName, e) => {
-      console.log(`Hello, ${userName}, there is an argument passed to this function`, e)
-   }
-
-
-
 
 	return (
 		<div className="home">
 			<h2>Homepage</h2>
+			<p>{name}</p>
 			{/* if the function doesnt passing an argument */}
 			<button onClick={handleClickOne}>Click Me</button>
-			<button onClick={(e) => {
+			{/* <button onClick={(e) => {
             handleClickTwo('Galih', e)
-         }}>Click Me Again</button>
+         }}>Click Me Again</button> */}
 		</div>
 	);
 };
