290cc7d5abe052a4a7a75063eeae2f6b57a4cf12
galihpermana29 <galihcbn123@gmail.com>
2021-02-04 09:59:22 +0700

    Props

---
+++ src/BlogList.js
@@ -0,0 +1,32 @@
+const BlogList = ({blogs, title}) => {
+   // Menggunakan props
+
+   // Nah tujuan menggunakan props itu biar komponennya lebih reuseable bisa digunakan di semua page, tinggal panggil tagnya saja, tanpa perlu menulis lagi logik dari code yang sama.
+   // nah, kita buat aja function baru, yang me return template html yang ingin kita jadikan reuseable. Nah bagaimana jika ternyata templatenya menampilkan data? seperti kasus dibawah ini?
+
+   // Kita gunakan props, jadi function ini menerima sebuah argument yang berasal dari komponen yang memanggilnya
+
+   //Home.js
+   // <BlogList blogs={blogs}/> properti blogs itu akan mengirimkan variabel blogs yang ada pada Home.js, lalu diterima oleh function ini BlogList lalu kita destructure langsung. Ingat bahwa properti blogs yang mengirimkan variabel blogs itu dan diterima oleh props pada function BlogList, akan menjadi sebuah objek, makanhya kita destructure objek pada parameter dari functionnya
+
+   // memanggil tagnya
+   // <BlogList blogs={blogs} title="Untitled"/>
+
+   // menerima propsnya pada function
+   // const BlogList = (props) => { const blogs = props.blogs; const title = props.title} atau bisa juga
+   // const BlogList = ({blogs, title}) => {console.log(blogs, title)}
+
+	return (
+		<div className="blog-list">
+         <h2>{title}</h2>
+			{blogs.map((blog) => (
+				<div className="blog-preview" key={blog.id}>
+					<h2>{blog.title}</h2>
+					<p>Written by {blog.author} </p>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 const Home = () => {
 	// let name = 'Galih';
    // console.log(useState('Galih'))
@@ -19,12 +20,8 @@ const Home = () => {
 
 	return (
 		<div className="home">
-         {blogs.map((blog) => (
-           <div className="blog-preview" key={blog.id}>
-              <h2>{blog.title}</h2>
-              <p>Written by {blog.author} </p>
-           </div>
-         ))}
+         <BlogList blogs={blogs} title="All Blogs!"/>
+
 			{/* <p>{name}</p> */}
 			{/* if the function doesnt passing an argument */}
 			{/* <button onClick={handleClickOne}>Click Me</button> */}
