6a64fa7bedfdd4b717ac124c9456dc30bf91c87d
galihpermana29 <galihcbn123@gmail.com>
2021-02-10 12:56:23 +0700

    React Router

--- src/App.js
+++ src/App.js
@@ -1,40 +1,32 @@
 // import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Create from './Create';
+
+// Untuk bisa ganti ganti page, kita butuh reactRouter yang mana harus di install dulu npm install react-router-dom@5
+// Lalu gunakan Router untuk membungkus elemennya, lalu gunakan switch jika bagian dari elemen tersebut yang ingin diganti ganti. Jangan lupa tambahkan tag route dahulu beserta pathnya yang mengarah ke tujuannya.
+
+// keyword exact itu digunakan agar dia hanya match jika namanya sama bukan beberapa saja yang sama
+// tanpa exact, /create === /. dengan exact, /create !== / sehingga respon yang dikembalikan bisa sesuai dengan pathnya.
 
 function App() {
-   // jadi pada react, kita buat nya itu komponen, yang mana misal navbar, kita buat jadi sebuah komponen, nah komponen itu kita buat dalam function, contohnya kaya komponen App ini. yang mana didalam function itu, kita bisa menuliskan apapun sintak js sperti biasanya, dan me return sebuah JSX atau html template yang berupa js.
-
-   // jangan lupa untuk bisa merender komponennya kedalam DOM, kita export dulu komponennya, lalu di bagian yang mau kita import, kita import aja komponen tadi. persis kaya js biasa
-   
-   // nah untuk menampilkan data yang dinamis, kita cukup kasih {namaVariabel} pakai kurung kurawal aja. ini bisa digunakan kecuali untuk object dan boolean
-   const user = 'Galih';
-   const greetings = `Halo ${user}`;
-   const age = 19;
-   const prodi = 'Teknik Komputer';
-   const khs = ['A', 'A', 'A'];
-   const ips = 4.0;
-   const univLink = 'https://siam.ub.ac.id/'
-
 	return (
-		<div className="App">
-         <Navbar />
-			<div className="content">
-            <Home />
-
-
-
-
-				{/* <h1>Biodata Mahasiswa</h1>
-            <h2>{greetings}</h2>
-            <p>Nama : {user}</p>
-            <p>Umur : {age}</p>
-            <p>Universitas : <a href={univLink} target="_blank">Univ Brawijaya</a></p>
-            <p>Prodi : {prodi}</p>
-            <p>Hasil Studi : {khs}</p>
-            <p>IPS : {ips}</p> */}
+		<Router>
+			<div className="App">
+				<Navbar />
+				<div className="content">
+					<Switch>
+						<Route exact path="/">
+							<Home />
+						</Route>
+						<Route path="/create">
+							<Create />
+						</Route>
+					</Switch>
+				</div>
 			</div>
-		</div>
+		</Router>
 	);
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,22 @@
+import { Link } from "react-router-dom";
+
+// sebagai pengganti anchor tag, kita gunakan tag Link yang mana mengintercept request agar tidak request lagi ke server. Jadi di handle oleh react nya, dan keyword href ganti sama to=""
+
 const Navbar = () => {
    return(
       <nav className="navbar">
          <h1>Galih Blog</h1>
          <div className="links">
-            <a href="">Home</a>
+            <Link to="/">Home</Link>
             {/* inline css using dynamic values sign {} baru didalamnya kirim sebuah js object {{
                stylenya
             }} */}
-            <a href="/create" style={{
+            <Link to="/create" style={{
                color: 'white',
                backgroundColor: '#f1356d',
                borderRadius: '8px',
                padding: '10px'
-            }}>New Blog</a>
+            }}>New Blog</Link>
          </div>
       </nav>
    )
