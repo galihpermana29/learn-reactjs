0a9470a12eb0a112f6cd5027011c9994142e5af1
galihpermana29 <galihcbn123@gmail.com>
2021-02-12 03:59:56 +0700

    finishhh

--- src/NotFound.js
+++ src/NotFound.js
@@ -3,8 +3,13 @@ import { Link } from "react-router-dom"
 const NotFound = () => {
    return (
       <div className="not-found">
-         <h2>Sorry</h2>
-         <p>This page cannot be found, please <Link to="/">go back to the Home Page</Link></p>
+         <h2 style={{
+            color: "#f1356d",
+            marginBottom: "20px"
+         }}>Sorry</h2>
+         <p>This page cannot be found, please <Link to="/" style={{
+            color: "#f1356d"
+         }}>go back to the Home Page</Link></p>
       </div>
    );
 }
